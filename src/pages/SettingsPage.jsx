729bdbe4822d6f0bee7dcbcedf5c126449a4c56e
Tyler Carr <tcarr@watchsystems.com>
2023-12-07 09:12:41 -0600

    added version and update app button

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,7 +1,13 @@
+import { Button } from "@mantine/core";
+
 function Settings() {
   return (
     <div className="grow max-h-[calc(100vh_-_92px)] overflow-auto">
-      <div className="flex justify-center">Settings</div>
+      <div className="flex justify-center text-xl font-semibold">Settings</div>
+      <div className="flex justify-center">Version 0.0.1</div>
+      <div className="flex justify-center mt-4">
+        <Button onClick={() => location.reload(true)}>Update</Button>
+      </div>
     </div>
   );
 }
