6896956910f189c3cec2f8bfc3e64843d8bc04dd
Tyler Carr <tcarr@watchsystems.com>
2023-12-07 09:23:35 -0600

    bump version, add real force reload

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,12 +1,22 @@
 import { Button } from "@mantine/core";
+import { unloadServiceWorkerAndRefresh } from "../utils/helpers";
 
 function Settings() {
+  async function updateApp() {
+    try {
+      await unloadServiceWorkerAndRefresh();
+    } finally {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      location.reload();
+    }
+  }
+
   return (
     <div className="grow max-h-[calc(100vh_-_92px)] overflow-auto">
       <div className="flex justify-center text-xl font-semibold">Settings</div>
-      <div className="flex justify-center">Version 0.0.2</div>
+      <div className="flex justify-center">Version 0.0.3</div>
       <div className="flex justify-center mt-4">
-        <Button onClick={() => location.reload(true)}>Update</Button>
+        <Button onClick={updateApp}>Update</Button>
       </div>
     </div>
   );
