5d5857ec4322f7b3ffb45a62258b8d0a44f88601
Tyler Carr <tcarr@watchsystems.com>
2023-12-10 00:54:57 -0600

    bump version

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -28,7 +28,7 @@ function Settings() {
   return (
     <div className="grow max-h-[calc(100vh_-_92px)] overflow-auto">
       <div className="flex justify-center text-xl font-semibold">Settings</div>
-      <div className="flex justify-center">Version 0.1.4</div>
+      <div className="flex justify-center">Version 0.2.0</div>
       <div className="flex justify-center mt-4 flex-col gap-2 items-center">
         <Button onClick={updateApp}>Update</Button>
         <Button onClick={fixSubreddits}>Fix Subreddits</Button>
