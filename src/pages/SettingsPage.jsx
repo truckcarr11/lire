084edfc16d04f55064bdbdc8daedf66bf467ba13
Tyler Carr <tcarr@watchsystems.com>
2023-12-12 15:56:02 -0600

    added button to re-subscribe to my subreddits after crash bump version

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,10 +1,11 @@
 import { Button, Switch } from "@mantine/core";
 import { unloadServiceWorkerAndRefresh } from "../utils/helpers";
 import { useAtom } from "jotai";
-import { settingsAtom } from "../state";
+import { settingsAtom, subscribedSubredditsAtom } from "../state";
 
 function Settings() {
   const [settings, setSettings] = useAtom(settingsAtom);
+  const [, setSubscribedSubreddits] = useAtom(subscribedSubredditsAtom);
 
   async function updateApp() {
     try {
@@ -19,12 +20,50 @@ function Settings() {
     setSettings({ ...settings, showVotingArrows: !settings.showVotingArrows });
   }
 
+  function tempFixSubreddits() {
+    setSubscribedSubreddits([
+      "nfl",
+      "thefinals",
+      "wow",
+      "competitivewow",
+      "teslamotors",
+      "teslalounge",
+      "webdev",
+      "reactjs",
+      "leagueoflegends",
+      "saints",
+      "spacex",
+      "electricvehicles",
+      "cars",
+      "movies",
+      "politics",
+      "fantasyfootball",
+      "gamedev",
+      "gameassets",
+      "apple",
+      "glassanimals",
+      "cruise",
+      "unity3d",
+      "unity",
+      "cfb",
+      "cloud9",
+      "chess",
+      "godot",
+      "taylorswift",
+      "outerwilds",
+      "appideas",
+      "louisiana",
+      "theboys",
+    ]);
+  }
+
   return (
     <div className="grow max-h-[calc(100vh_-_92px)] overflow-auto">
       <div className="flex justify-center text-xl font-semibold">Settings</div>
-      <div className="flex justify-center">Version 0.3.1</div>
+      <div className="flex justify-center">Version 0.3.2</div>
       <div className="flex justify-center mt-4 flex-col gap-2 items-center">
         <Button onClick={updateApp}>Update</Button>
+        <Button onClick={tempFixSubreddits}>Fix Subreddits</Button>
       </div>
       <div className="flex m-4 items-center justify-between">
         <p>Show Voting Arrow</p>
