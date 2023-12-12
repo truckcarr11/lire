a4d1898d9005799b8ff533275d26061a03f8fc59
Tyler Carr <tcarr@watchsystems.com>
2023-12-12 15:31:34 -0600

    reduced min swipe distance to make easier to do

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,4 +9,4 @@ export const HEADER_NAV_PXS = 80;
 export const TRIGGER_THRESHOLD = 200;
 export const SHOW_INDICATOR_THRESHOLD = 100;
 
-export const MIN_SWIPE_DISTANCE = 200;
+export const MIN_SWIPE_DISTANCE = 100;
